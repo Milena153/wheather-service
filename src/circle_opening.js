f96929c1cf88e5a3cd6845cba314e75731ca9749
Raturchik <artyrkafarov@example.com>
2025-04-12 17:57:12 +0300

    vse po novoy :)

--- src/circle_opening.js
+++ src/circle_opening.js
@@ -1,40 +1,5 @@
-const audio_but = document.querySelector(".audio");
-const cover_div = audio_but.firstElementChild;
-const title_music = cover_div.firstElementChild;
-const music_list = cover_div.lastElementChild;
-const icon = cover_div.querySelector(".visible_icon");
+import {createAudioPlayer} from './createAudioPlayer'
 
+createAudioPlayer();
 
-let buttons = document.querySelectorAll(".hidden_but");
-console.log(buttons);
-
-
-audio_but.addEventListener("click", (event) => {
-
-
-    if (!event.target.classList.contains("hidden-button")) {
-        audio_but.classList.toggle("open");
-        title_music.classList.toggle("open_title");
-        music_list.classList.toggle("visible");
-        icon.classList.toggle("hidden_icon");
-        
-
-        if (audio_but.classList.contains("open")) {
-          buttons.forEach(element => {
-            element.classList.replace("hidden_but", "visible_but");
-          });
-        } else {
-          buttons.forEach(element => {
-            element.classList.replace("visible_but", "hidden_but");
-          });
-        }
-    }
-});
-
-
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      event.stopPropagation();
-    });
-  });
 
---
+++ src/createAudioPlayer.js
@@ -0,0 +1,14 @@
+export function createAudioPlayer() {
+    document.body.innerHTML = `
+        <div class="audio_module open">
+            <h3 class="closed_title">music list</h3>
+            <ul class="hidden_ul">
+                <button class="hidden_but"></button>
+                <button class="hidden_but"></button>
+                <button class="hidden_but"></button>
+                <button class="hidden_but"></button>
+            </ul>
+        </div>
+    `
+}
+
