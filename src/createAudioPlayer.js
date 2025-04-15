67bb3a0aa015b078baac5d99a6e914c25a144dc0
Raturchik <artyrkafarov@gmail.com>
2025-04-15 18:46:05 +0300

    code fix #2

--- src/createAudioPlayer.js
+++ src/createAudioPlayer.js
@@ -1,7 +1,10 @@
 export function createAudioPlayer() {
     document.body.innerHTML = `
     <button class="music_button">
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAACCklEQVR4nO3XO6wNURjF8fGIqOg8G3Lbq9KQSDQKJKJSeRQKKhGPRBA3GoVEhIQeNQUKN0SBRqHQUNCoSLxC4RG58ZMd+x7j5Nx7ZsbMmJPsfzPJmcn3rTVn7fn2zrJEIpFI5MBq7MJlPMVXnMm6CBZgPQ7jBl4bzPesC2AptuE07sW3W4j/IXYu1mA/ruJlQa2fcAcTrRvAChzHJD4XFPwCV7AP48F4rl57BrAXP4aI/YZHOIvtWDKkZjsGsBE/BwgOi/I6DmFdWLQl6/ZoTv3vRmFBTvMYO7Gqhro96lE6c6OPf1pZVuMc6FGf2sFNKzXCfKzFQVzDq3ytqnVLU7QRFmNT5+aAAY3iHBjPzYHwuezmHPA3E1FAEDIac0Bxwpx4govY0aU5MBNvcBvHsAELq9bNmqRP9KVRnAM9RqFua40kAwVJ/8AoRggrcSqeD97H7fhb3McBLOqsARwtsA9610kDuKACWRcMYGufrmdx7x/O0nPidXf8vR4DdWa17+R2M5wHZjkn3PpnA3Vnte/kNjbk2bHcs1NVxNee1X8w8KGs+Eayiru5R0NEZotQ2MVOM1nWQCNZxZY+v8/jiwgvZF5cb3vi73k2lzXQWFZxXjnOlRLfRlbjx+HLEOHh/pHS4tvKKpbjJB7Gz/JUvD7AiXC/kvhWs9okrWS1aRrPahs0mtVEIpH4BexcS0yat4SyAAAAAElFTkSuQmCC" alt="musical-notes">
+        <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+            <path d="m11 21.001v-14.001c0-.453.305-.85.742-.966l15-4c.301-.08.621-.016.867.173.247.189.391.482.391.793v18c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c1.125 0 2.164.372 3 1.001v-7.699l-13 3.466v12.232c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c1.125 0 2.164.372 3 1.001zm-3 .999c1.656 0 3 1.344 3 3s-1.344 3-3 3-3-1.344-3-3 1.344-3 3-3zm15-4c1.656 0 3 1.344 3 3s-1.344 3-3 3-3-1.344-3-3 1.344-3 3-3zm3-10.768v-2.93l-13 3.466v2.93z" fill="rgb(255, 255, 255)">
+            </path>
+        </svg>
     </button>
 
     <div class="audio_module closed">
