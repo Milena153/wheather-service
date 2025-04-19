ef47e0b64a55b88309e30bd9c3def0713fa38d7e
Raturchik <artyrkafarov@example.com>
2025-04-19 18:04:30 +0300

    started working on audio

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const data = [{
+    title: "The sound of the weather: rain dripping on dry leaves in the park",
+    src: "../public/music/28f2bf70e8d0701.mp3",
+    description: "The soft soothing sounds of rain falling on dry leaves",
+}]
+
+export const firstSong = data[0];
