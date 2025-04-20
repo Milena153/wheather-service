9a67fdcc8d1c2c537ce2fac77491fa346f8d1ebd
Raturchik <artyrkafarov@gmail.com>
2025-04-20 12:57:54 +0300

    podumal

--- src/createAudioPlayer.js
+++ src/createAudioPlayer.js
@@ -1,5 +1,8 @@
+import { firstSong } from "./data"
+
 export function createAudioPlayer() {
-    document.body.innerHTML = `
+    const audioContainer = document.querySelector('.audioContainer') 
+    audioContainer.innerHTML = `
     <button class="music_button">
         <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
             <path d="m11 21.001v-14.001c0-.453.305-.85.742-.966l15-4c.301-.08.621-.016.867.173.247.189.391.482.391.793v18c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c1.125 0 2.164.372 3 1.001v-7.699l-13 3.466v12.232c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c1.125 0 2.164.372 3 1.001zm-3 .999c1.656 0 3 1.344 3 3s-1.344 3-3 3-3-1.344-3-3 1.344-3 3-3zm15-4c1.656 0 3 1.344 3 3s-1.344 3-3 3-3-1.344-3-3 1.344-3 3-3zm3-10.768v-2.93l-13 3.466v2.93z" fill="rgb(255, 255, 255)">
@@ -8,15 +11,18 @@ export function createAudioPlayer() {
     </button>
 
     <div class="audio_module closed">
-        <h3 class="title">music list</h3>
-        <ul class="song_list">
-            <button class="song_button"></button>
-            <button class="song_button"></button>
-            <button class="song_button"></button>
-            <button class="song_button"></button>
-        </ul>
-        <button class="back_button">back</button>
+        <div class="module_content closed">
+            <h3 class="title">music list</h3>
+                <figure id="audioPlayer" style="width:fit-content">
+                <figcaption>Listen to the T-Rex:</figcaption>
+                <audio  controls autoplay>
+                    <source src=${firstSong.src} type="audio/mp3">          
+                </audio>
+                </figure>
+                <p style="color:white">${firstSong.title}</p>
+            
+            <button class="back_button">back</button>
+        </div>
     </div>
     `
 }
-
--- src/openAudioModule.js
+++ src/openAudioModule.js
@@ -1,20 +1,26 @@
 export function openAudioModule() {
-const music_button = document.querySelector('.music_button');
-const music_module = document.querySelector('.audio_module');   
-const back_button = document.querySelector(".back_button");
-
-music_button.addEventListener("click", () => {
-
-    if(music_module.classList.contains("closed")){
-        music_module.classList.remove("closed");
-        music_button.classList.add("closed");
-    }else{
-        music_module.classList.add("closed");
-        music_button.classList.remove("closed");
-    }})
-
-    back_button.addEventListener("click", () => {
-        music_button.classList.remove("closed");
-        music_module.classList.add("closed");
-    })
-}
+    const music_button = document.querySelector('.music_button');
+    const music_module = document.querySelector('.audio_module');   
+    const back_button = document.querySelector(".back_button");
+    const module_content = document.querySelector(".module_content");
+    console.log(module_content);
+     
+    
+    music_button.addEventListener("click", () => {
+    
+        if(music_module.classList.contains("closed")){
+            module_content.classList.remove("closed");
+            music_module.classList.remove("closed");
+            music_button.classList.add("closed");
+        }else{
+            module_content.classList.add("closed");
+            music_module.classList.add("closed");
+            music_button.classList.remove("closed");
+        }})
+    
+        back_button.addEventListener("click", () => {
+            module_content.classList.add("closed");
+            music_button.classList.remove("closed");
+            music_module.classList.add("closed");
+        })
+    }
